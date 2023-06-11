232f7c0e57d48a8cd3624ad30400030a5576caee
Majorx234 <majorx234@googlemail.com>
2023-06-11 23:55:36 +0200

    Add worker base class

---
+++ include/http_worker.hpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <chrono>
+#include <functional>
+#include <optional>
+
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/optional/optional.hpp>
+
+#ifndef HTTP_WORKER_HPP
+#define HTTP_WORKER_HPP
+
+class HttpWorker {
+  
+}
+
+#endif // HTTP_WORKER_HPP
